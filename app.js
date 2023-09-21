31f753de21063a477952a5c39451738086cabc61
josecardozoif <jose.araujo6@estudante.ifms.edu.br>
2023-09-21 14:34:09 -0400

    feat: add /meunome GET

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.get('/req', (req, res) => {
     res.send('Yo!')
 })
 
+app.get('/meunome', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Meu nome é José Eduardo Cardozo Araújo!')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
